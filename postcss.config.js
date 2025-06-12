7232e6664d8ffee7a4519aa156a8caee4eac3666
James Bradford <jabradford@microsoft.com>
2025-06-12 16:57:40 -0700

    tailwind setup

--- postcss.config.js
+++ postcss.config.js
@@ -2,6 +2,7 @@ module.exports = {
   plugins: [
     require('postcss-import'),
     require('postcss-url')({ url: 'rebase' }),
+    require('@tailwindcss/postcss'),
     // Add more plugins here if needed
   ]
 }; 
---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./styles/**/*.{css,js}",
+    "./**/*.html"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+} 
