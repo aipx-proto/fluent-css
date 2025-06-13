b5d5bef9cbe88089b2cd735249c8f354ffde835e
James Bradford <jabradford@microsoft.com>
2025-06-13 14:12:17 -0700

    delete postcss for simplicity

--- postcss.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  plugins: [
-    // require('postcss-import'),
-    require('@tailwindcss/postcss'),
-    // require('postcss-url')({ url: 'rebase' }),
-    // Add more plugins here if needed
-  ]
-}; 
