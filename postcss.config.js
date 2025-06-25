c43bc4fabe2134823d4f165de0a9fe252fe37411
James Bradford <jabradford@microsoft.com>
2025-06-25 09:59:25 -0700

    fluent.tailwind.css is a dead end because its not `@import`able at js browser runtime...

--- postcss.config.js
+++ postcss.config.js
@@ -2,15 +2,16 @@ const ensureTailwindImport = ".ensure-tailwind-import";
 
 module.exports = {
   plugins: [
-    // require('postcss-import'),
-    require('./plugins/extract-custom-properties')({
-      scanDir: './styles',
-      ruleName: ensureTailwindImport
-    }),
-    require('@tailwindcss/postcss'),
-    require('css-byebye')({
-      rulesToRemove: [ensureTailwindImport]
-    }),
+    require('postcss-import'),
+    require('postcss-discard-comments'),
     // require('postcss-url')({ url: 'rebase' }),
+    // require('./plugins/extract-custom-properties')({
+    //   scanDir: './styles',
+    //   ruleName: ensureTailwindImport
+    // }),
+    // require('@tailwindcss/postcss'),
+    // require('css-byebye')({
+    //   rulesToRemove: [ensureTailwindImport]
+    // }),
   ]
 }; 
